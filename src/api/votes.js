b640d6091abbb286b68f731d10e67230ceb2b2ff
vinayak-trivedi <90315175+vinayak-trivedi@users.noreply.github.com>
2023-12-31 08:53:41 +0530

    votes ordering and vote count update logic (#4)

--- src/api/votes.js
+++ src/api/votes.js
@@ -1,8 +1,21 @@
-import { VOTES } from '../mocks/votes'
+import { VOTES } from '../mocks/votes';
+
+let votes = [...VOTES];
 
 export const getVotes = () => {
+  const randomIndex = Math.floor(Math.random() * VOTES.length);
+  const randomVotes = Math.floor(Math.random() * 5) + 1;
+
+  const updatedVotes = [...votes];
+  updatedVotes[randomIndex] = {
+    ...updatedVotes[randomIndex],
+    votes: (updatedVotes[randomIndex].votes || 0) + randomVotes,
+  };
+  votes = updatedVotes;
+
   return {
     success: true,
-    data: VOTES
-  }
-}
+    data: updatedVotes.sort((a, b) => b.votes - a.votes),
+  };
+};
+
