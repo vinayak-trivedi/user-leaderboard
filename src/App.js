b44e537d8d53050584adf00fccfadda04dd6e5f5
vinayak <trivedivinayak57@gmail.com>
2023-12-31 08:45:05 +0530

    votes ordering and vote count update logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getVotes } from './api/votes';
 import VotesDisplayItem from './components/VotesDisplayItem';
 
 function App() {
-  const { data: votes } = getVotes();
+  const [votes, setVotes] = useState([]);
   const votesItems = votes?.map((vote) => (
-    <VotesDisplayItem votes={vote.votes} userId={vote.userId} />
+    <VotesDisplayItem
+      key={vote.userId}
+      votes={vote.votes}
+      userId={vote.userId}
+    />
   ));
+
+  useEffect(() => {
+    const { data } = getVotes();
+    setVotes(data);
+  }, []);
+
+  useEffect(() => {
+    let interval;
+    interval = setInterval(() => {
+      const { data } = getVotes();
+      setVotes(data);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
       <h1>Leaderboard</h1>
@@ -15,9 +36,7 @@ function App() {
           <h2>User</h2>
           <p className="votes-details__header-text">Votes</p>
         </header>
-        <div className="votes-details__votes-item-container">
-          {votesItems}
-        </div>
+        <div className="votes-details__votes-item-container">{votesItems}</div>
       </section>
     </div>
   );
